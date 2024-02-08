e7b2050160fb61d5b63a890fd63512b040e66f88
shiinano <129734246+shiinano-mommy@users.noreply.github.com>
2024-02-08 13:12:19 +0100

    Update app.js

--- script/app.js
+++ script/app.js
@@ -5,19 +5,19 @@ fetch(`https://api.lanyard.rest/v1/users/${discordID}`)
   .then((response) => response.json())
   .then((e) => { 
     if(e.data["discord_user"]){
-        if (e.data.discord_status == "online") {
-            document.getElementById("discord-status-highlight").innerText = "Online";
+        if (e.data.discord_status === "online") {
+            document.getElementById("discord-status-highlight").innerText = "online";
             document.getElementById("discord-status-highlight").style.color = "#23a55a";
           }
-          else if (e.data.discord_status == "idle"){
-            document.getElementById("discord-status-highlight").innerText = "away from keyboard";
+          else if (e.data.discord_status === "idle"){
+            document.getElementById("discord-status-highlight").innerText = "idle";
             document.getElementById("discord-status-highlight").style.color = "#f0b232";
           }
-          else if (e.data.discord_status == "dnd"){
-            document.getElementById("discord-status-highlight").innerText = "on do not disturb";
+          else if (e.data.discord_status === "dnd"){
+            document.getElementById("discord-status-highlight").innerText = "on Do not disturb";
             document.getElementById("discord-status-highlight").style.color = "#f23f43";
           }
-          else if (e.data.discord_status == "offline"){
+          else if (e.data.discord_status === "offline"){
             document.getElementById("discord-status-highlight").innerText = "Offline";
             document.getElementById("discord-status-highlight").style.color = "#80848e";
           }
@@ -37,7 +37,7 @@ fetch(`https://api.lanyard.rest/v1/users/${discordID}`)
 webSocket.addEventListener("message", (event) => {
     let data = JSON.parse(event.data);
   
-    if (event.data == '{"op":1,"d":{"heartbeat_interval":30000}}') {
+    if (event.data === '{"op":1,"d":{"heartbeat_interval":30000}}') {
       webSocket.send(
         JSON.stringify({
           op: 2,
@@ -57,7 +57,7 @@ webSocket.addEventListener("message", (event) => {
         );
       }, 30000);
     }
-    if (data.t == "PRESENCE_UPDATE") {
+    if (data.t === "PRESENCE_UPDATE") {
       if (data.d.spotify) {
         document.getElementById("spotify-song").innerText = "listening to " + data.d.spotify.song;
         document.getElementById("spotify-artist").innerText = " by " + data.d.spotify.artist.replaceAll(";", ",");
@@ -68,3 +68,16 @@ webSocket.addEventListener("message", (event) => {
         }
     }
 });
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// right click disable
+document.onkeydown = function(event) {
+    if (event.keyCode === 123 || isCtrlShiftKey(event, 'I') || isCtrlShiftKey(event, 'J') ||
+        isCtrlShiftKey(event, 'C') || (event.ctrlKey && event.keyCode === 'U'.charCodeAt(0))) {
+        return false;
+    }
+};
+
+function isCtrlShiftKey(event, key) {
+    return event.ctrlKey && event.shiftKey && event.keyCode === key.charCodeAt(0);
+}
